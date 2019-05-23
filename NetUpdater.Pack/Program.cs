234ed5e2cda9917d3c141637372c2b3fbcbd78f2
Waldemar Tomme <waldetomme@gmail.com>
2019-05-23 10:14:03 +0200

    Added basic project layout

---
+++ NetUpdater.Pack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetUpdater.Pack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
