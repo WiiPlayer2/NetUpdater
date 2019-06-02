8b4740d17f42abf5b5094c6760413dd35e5f9d05
Waldemar Tomme <waldetomme@gmail.com>
2019-06-02 10:55:52 +0200

    Added creation of diff

---
+++ NetUpdater.Core/Diff.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NetUpdater.Core
+{
+    public class Diff
+    {
+        #region Public Constructors
+
+        public Diff(IEnumerable<Uri> updates, IEnumerable<Uri> deletions)
+        {
+            Updates = updates.ToList();
+            Deletions = deletions.ToList();
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public IEnumerable<Uri> Deletions { get; }
+
+        public IEnumerable<Uri> Updates { get; }
+
+        #endregion Public Properties
+    }
+}
--- NetUpdater.Core/Manifest.cs
+++ NetUpdater.Core/Manifest.cs
@@ -14,6 +14,7 @@ namespace NetUpdater.Core
             Channel = channel;
             Version = version;
             Hashes = hashes.ToList();
+            HashMap = Hashes.ToDictionary(o => o.Item2, o => o.Item1);
         }
 
         #endregion Public Constructors
@@ -24,8 +25,23 @@ namespace NetUpdater.Core
 
         public IEnumerable<(string, Uri)> Hashes { get; }
 
+        public IReadOnlyDictionary<Uri, string> HashMap { get; }
+
         public Version Version { get; }
 
         #endregion Public Properties
+
+        #region Public Methods
+
+        public Diff CreateDiff(Manifest target)
+        {
+            var newFiles = target.HashMap.Keys.Except(HashMap.Keys);
+            var oldFiles = HashMap.Keys.Except(target.HashMap.Keys);
+            var updatedFiles = HashMap.Keys.Intersect(target.HashMap.Keys).Where(o => HashMap[o] != target.HashMap[o]);
+
+            return new Diff(newFiles.Concat(updatedFiles), oldFiles);
+        }
+
+        #endregion Public Methods
     }
 }
