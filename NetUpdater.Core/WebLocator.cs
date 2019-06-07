fa02f4a4835cc52a77bc8c26e2c05b5453d18cfb
Waldemar Tomme <waldetomme@gmail.com>
2019-06-07 18:12:55 +0200

    Added simple WebLocator

---
+++ NetUpdater.Core/WebLocator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetUpdater.Core
+{
+    public class WebLocator : IUriLocator
+    {
+        #region Public Fields
+
+        public static readonly WebLocator Instance = new WebLocator();
+
+        #endregion Public Fields
+
+        #region Public Methods
+
+        public async Task<Stream> Locate(Uri uri)
+        {
+            var request = WebRequest.CreateHttp(uri);
+            var response = await request.GetResponseAsync();
+            return response.GetResponseStream();
+        }
+
+        #endregion Public Methods
+    }
+}
