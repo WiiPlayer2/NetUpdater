098db781e6048ea6a3ac7f47a5486a17cd10530e
Waldemar Tomme <waldetomme@gmail.com>
2019-06-07 18:52:44 +0200

    Added simple cli tool

---
+++ NetUpdater.Cli/Program.cs
@@ -0,0 +1,37 @@
+﻿using CommandLine;
+using MicroElements.Functional;
+using NetUpdater.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace NetUpdater.Cli
+{
+    internal class Program
+    {
+        #region Private Methods
+
+        private static async Task Main(string[] args)
+        {
+            await Parser.Default.ParseArguments<UpdateOptions>(args).MapResult(RunUpdate, RunError);
+        }
+
+        private static Task RunError(IEnumerable<Error> arg) => Task.CompletedTask;
+
+        private static async Task RunUpdate(UpdateOptions options)
+        {
+            if (options.Pid.HasValue)
+            {
+                Console.WriteLine($"Waiting for process {options.Pid} to exit...");
+                Process.GetProcessById(options.Pid.Value).WaitForExit();
+            }
+
+            Console.WriteLine($"Running update in \"{options.ApplicationPath}\"...");
+            var updater = new Updater(WebLocator.Instance, options.ApplicationPath.LocalPath, options.ManifestName, OptionNone.Default);
+            await updater.Update(options.UpdateUrl);
+        }
+
+        #endregion Private Methods
+    }
+}
---
+++ NetUpdater.Cli/UpdateOptions.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetUpdater.Cli
+{
+    [Verb("update", HelpText = "Updates an application packed with NetUpdater.")]
+    internal class UpdateOptions
+    {
+        #region Public Properties
+
+        [Option('a', "applicationPath", Required = true, HelpText = "Sets the application path.")]
+        public Uri ApplicationPath { get; set; }
+
+        [Option('m', "manifestName", Required = true, HelpText = "Sets the manifest name of the application.")]
+        public string ManifestName { get; set; }
+
+        [Option('p', "pid", HelpText = "Sets the pid of the process to wait for exit before updating.")]
+        public int? Pid { get; set; }
+
+        [Option('u', "updateUrl", Required = true, HelpText = "Sets the update url to download contents from.")]
+        public Uri UpdateUrl { get; set; }
+
+        #endregion Public Properties
+    }
+}
